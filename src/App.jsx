446042ae571f883a94ed5235dd770fae9b3429dc
Ramla-Razykan <ramla.eman.ra@gmail.com>
2025-01-23 13:19:58 +0500

    UI complete

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,17 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
   useEffect(() => {
-    document.title = `${count}`;
+    document.title = `Counter: ${count}`;
   });
 
   return (
     <>
-      <div className='w-[30%] bg-slate-100 p-4 flex flex-col items-center justify-center space-y-3'>
-        <h1 className='text-[3rem]'>Counter with Side Effect</h1>
+      <div className='w-[30%] bg-[#858FFD] p-4 rounded-2xl text-white flex flex-col items-center justify-center space-y-3'>
+        <h1 className='text-[2.8rem] font-bold'>Counter With Side Effect</h1>
         <i class="bi bi-arrow-down text-[1.8rem]"></i>
-        <button onClick={() => setCount(count + 1)} className='border-2 text-[1.4rem] py-1 px-6 rounded-lg'>Click: {count}</button>
-        <button onClick={() => setCount(0)} className="border-2 text-[1.4rem] py-1 px-6 rounded-lg" >Reset</button>
-        <p className='text-[1.8rem]'>Check The <span className='bg-yellow-200 px-2 rounded-e-md'>Document Title</span></p>
+        <button onClick={() => setCount(count + 1)} className='border-2 text-[1.4rem] py-1 px-6 rounded-lg hover:bg-[#696FF2]'>Click: {count}</button>
+        <button onClick={() => setCount(0)} className="border-2 text-[1.4rem] py-1 px-6 rounded-lg hover:bg-[#696FF2]" >Reset</button>
+        <p className='text-[1.8rem]'>Check The <span className='bg-yellow-200 text-[#858FFD] px-2'>Document Title</span></p>
       </div>
     </>
   )
